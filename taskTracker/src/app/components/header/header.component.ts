11936cbf77adc7552523bdd0160d598f2f07a906
reftene <reftene@deloittece.com>
2022-01-06 23:28:01 +0200

    good practice app

---
+++ taskTracker/src/app/components/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  btnText: string = 'Add task';
+  btnColor: string = 'green';
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value: boolean) => {
+        this.showAddTask = value;
+      });
+  }
+
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+}
